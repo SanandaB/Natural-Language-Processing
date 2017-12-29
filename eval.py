da4d6bf5702f37189469a0c47a19e175def56d8f
SanandaB <sananda.banerjee@hotmail.com>
2017-12-29 15:46:09 -0700

    Add eval script

---
+++ eval.py
@@ -0,0 +1,24 @@
+import sys
+
+def eval(keys, predictions):
+    """ Simple minded eval system: file1 is gold standard, file2 is system outputs. Result is straight accuracy. """
+
+    count = 0.0
+    correct = 0.0
+
+    for key, prediction in zip(keys, predictions):
+        key = key.strip()
+        prediction = prediction.strip()
+        if key == '': continue
+        count += 1
+        if key == prediction: correct += 1
+
+    #print correct
+
+    print("Evaluated ", count, " tags.")
+    print("Accuracy is: ", correct / count)
+
+if __name__ == "__main__":
+     keys = open(sys.argv[1])
+     predictions = open(sys.argv[2])
+     eval(keys, predictions)
